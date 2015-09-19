bbce7f4e8176ae45090fd42d7d1251626c32c257
Heikki Pesonen <heikki.pesonen@luotta.fi>
2015-09-19 09:00:33 +0300

    updated data models

--- server/testdata.js
+++ server/testdata.js
@@ -10,21 +10,53 @@ Meteor.methods({
 createTestData = function () {
   Locations.remove({});
   Products.remove({});
+  Producers.remove({});
+  Users.remove({});
+
+  var producers = [];
+  var users = [];
+
+  _.times(50, function () {
+    var userId = Users.insert({
+      name: chance.name(),
+      level: _.sample(Users.LEVELS),
+      address: chance.address()
+    });
+
+    users.push(userId);
+  });
+
+  _.times(50, function () {
+    var producerId = Producers.insert({
+      name: chance.sentence({words:2}),
+      description: chance.paragraph(),
+      type: _.sample(Producers.TYPES),
+      address: chance.address(),
+      latitude: Math.random()*6 + 60,
+      longitude: Math.random()*8 + 22
+    });
+
+    producers.push(producerId);
+  });
+
 
   _.times(100, function () {
     var locationId = Locations.insert({
       name: chance.word(),
       latitude: Math.random()*6 + 60,
       longitude: Math.random()*8 + 22,
-      type: _.sample(Locations.TYPES)
+      start_datetime: chance.hammertime(),
+      end_datetime: chance.hammertime(),
+      type: _.sample(Locations.TYPES),
+      owner: _.sample(users)
     });
 
-    _.times(chance.integer({min: 1, max: 10}), function () {
+   _.times(chance.integer({min: 1, max: 10}), function () {
       Products.insert({
         location_id: locationId,
+        producer_id: _.sample(producers),
         name: chance.sentence({words: 2}),
         description: chance.paragraph({sentences:1}),
-        company: chance.sentence({words: 3}),
         category: _.sample(Products.CATEGORIES),
         price: chance.floating({min: 0.5, max: 10}),
         unit: _.sample(Products.UNITS),
